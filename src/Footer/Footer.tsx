75c4d46ca6f9cfaabd822e8fc90ae00489976569
Kai Delay <kbde@riseup.net>
2023-12-16 14:18:58 +0100

    code refactor, added tsconfig

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <div className="flex row center between">
+                <img src="keys.png" alt="" height={'50px'} style={{ width: '120px' }} />
+                <section>
+                    <h4 className="secondary helvetica">Move tiles in grid</h4>
+                    <h4 className="secondary helvetica">to order them from <em className="primary">1 to 15.</em></h4>
+                </section>
+            </div>
+
+            <section>
+                <p className="primary helvetica">Made with <em className="red">❤</em> by kai</p>
+                <p className="primary helvetica">This project is open source, visit the <a className="weight bold" href="google.com">repo.</a></p>
+            </section>
+
+            <div className="gap halfrem flex row center">
+                <p>GI</p>
+                <p>IN</p>
+                <p>TW</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+export {Footer}
---
+++ src/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1 className="cursive">15 Puzzle</h1>
+        </header>
+    )
+}
+
+export default Header
